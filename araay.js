9e0f3e3163024d45ccea83e76563af961ac0fe7d
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2022-08-03 07:16:33 +0600

    insert & delete

---
+++ araay.js
@@ -0,0 +1,10 @@
+var tableitem = ['bottole', 'pen','mouse', 'sunglass','paper'];
+
+
+ tableitem[4] ='notepad';
+
+var penIndex = tableitem.indexOf('pen');
+ console.log(tableitem);
+ console.log(penIndex);
+ 
+
--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log(34);
+const arr = ['a', 'b', 'c'];
+var index;
+
+for ( index = 0; index < arr.length; index++) {
+
+
+    console.log(arr);
+
+
+    if (arr[index] === 'a') {
+        arr[index] = 'z';
+        break;
+    }
+
+
+}
+
+console.log(arr);
+
+// if ses
+
+ arr = ['a', 'b', 'c'];
+
+ index = arr.indexOf('a'); // 👉️  0
+
+arr.splice(index, 1, 'z');
+
+console.log(arr);
+
+// -splice use-
+
+ arr = ['a', 'b', 'c'];
+
+ index = arr.indexOf('a'); // 👉️  0
+
+if (index !== -1) {
+  arr[index] = 'z';
+}
+
+console.log(arr);
+
+// --index khuje na pao--
