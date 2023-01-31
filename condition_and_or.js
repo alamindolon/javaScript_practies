dd7ef8aa2bc0cd3c1edbf6db86df944b34b8b16d
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-01-31 11:45:19 +0600

    and or added

---
+++ condition_and_or.js
@@ -0,0 +1,16 @@
+var isgraduate = true;
+var salary = 50000;
+var car = 1;
+
+if(isgraduate === true && salary >65000)
+{
+    console.log('eso prem kori')
+}
+else if(isgraduate === true || salary >65500)
+{
+    console.log('eso biya kori')
+}
+else
+{
+    console.log('tmr friend a numver deo')
+}
