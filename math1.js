272aa9f24deabffda1a25270cc7afd2aeaaa3d7a
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-01-31 13:42:37 +0600

    math 2 done

---
+++ math1.js
@@ -0,0 +1,6 @@
+var money = 1000;
+var apple = 500;
+var orange  = 300;
+var buy = apple + orange;
+var cash = money- buy;
+console.log(cash);
---
+++ math2.js
@@ -0,0 +1,5 @@
+var one = 'I am going to be'
+var two = 'an awesome web devloper'
+
+var sum = one + ' ' + two;
+console.log(sum);
