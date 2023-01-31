15b2cc432434dd887b538d92cd23b606fba57d94
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-01-31 13:11:29 +0600

    break and continue

--- loop.js
+++ loop.js
@@ -7,9 +7,33 @@
 
 // }
 
-var number = 1;
-while(number<10)
+// var number = 1;
+// while(number<10)
+// {
+//     console.log(number);
+//     number=number+2;
+// }
+
+// var number = [34,36,45,56,60,66,45];
+
+// for(var i=0; i<number.length; i++)
+// {
+//     var numbers = number[i];
+//     if(numbers>50)
+//     {
+//         break;
+//     }
+//     console.log(numbers)
+// }
+
+var number = [34,36,45,56,60,66,45];
+
+for(var i=0; i<number.length; i++)
 {
-    console.log(number);
-    number=number+2;
+    var numbers = number[i];
+    if(numbers>50)
+    {
+        continue;
+    }
+    console.log(numbers)
 }
